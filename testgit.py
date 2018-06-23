7b5d14def47d20f5375f438f75a5a623896be2ce
viveksingh407 <viveksingh407@hotmail.com>
2018-06-23 10:53:00 +0530

    next commit

--- testgit.py
+++ testgit.py
@@ -1,1 +1,10 @@
-print 'Hello World'
+import os
+
+def print_hello_world():
+     test = 'Hello World'
+     with open(os.path.join('D:\\TestApplications\\TestGit', "test_hello_world.txt"), 'wb') as test_file:
+        test_file.write(test)
+
+if __name__ == '__main__':
+    print_hello_world()
+
